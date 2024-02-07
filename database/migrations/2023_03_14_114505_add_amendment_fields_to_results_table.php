5b84686edbddc73020b3283f1cb6243ae25393fb
ked-k <kedkayz@gmail.com>
2024-02-07 21:26:38 +0300

    Update 2023_03_14_114505_add_amendment_fields_to_results_table.php

--- database/migrations/2023_03_14_114505_add_amendment_fields_to_results_table.php
+++ database/migrations/2023_03_14_114505_add_amendment_fields_to_results_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::table('test_results', function (Blueprint $table) {
-            $table->string('amended_state')->default(false)->after('kit_expiry_date');
-            $table->string('amendment_type')->nullable()->after('amended_state');
-            $table->text('original_results')->nullable()->after('amendment_type');
-            $table->text('amendment_comment')->nullable()->after('original_results');
-            $table->unsignedBigInteger('amended_by')->nullable()->after('amendment_comment');
-            $table->dateTime('amended_at')->nullable()->after('amended_by');
+            // $table->string('amended_state')->default(false)->after('kit_expiry_date');
+            // $table->string('amendment_type')->nullable()->after('amended_state');
+            // $table->text('original_results')->nullable()->after('amendment_type');
+            // $table->text('amendment_comment')->nullable()->after('original_results');
+            // $table->unsignedBigInteger('amended_by')->nullable()->after('amendment_comment');
+            // $table->dateTime('amended_at')->nullable()->after('amended_by');
         });
     }
 
