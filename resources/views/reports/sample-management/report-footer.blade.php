c3d34ab214eadfbf68c6717de57c154112850e5c
unknown <98885599+crsSupport@users.noreply.github.com>
2024-11-04 17:12:33 +0300

    Update report-footer.blade.php

--- resources/views/reports/sample-management/report-footer.blade.php
+++ resources/views/reports/sample-management/report-footer.blade.php
@@ -11,13 +11,16 @@
                 </p>
 
             </td>
-            <td  style="font-size:10px; text-align:center">
-                <img width="186px" height="112px" src="{{asset('autolab-assets/images/stamp.png')}}" alt="">
+            <td style="font-size:10px; text-align:center">
+                <img width="180px" height="100px" src="{{ asset('autolab-assets/images/stamp.png') }}" alt="">
                 <div style=" position: absolute; top: 34%; left: 77%;color: red">
-                    <h2 style="text-transform: uppercase; font-family: 'Arial Black', serif; font-size: 16px;font-weight: bold;">{{date('d M Y')}}</h2></div>
+                    <h2
+                        style="text-transform: uppercase; font-family: 'Arial Black', serif; font-size: 16px;font-weight: bold;">
+                        {{ date('d M Y') }}</h2>
+                </div>
             </td>
         </tr>
-        <tr>
+        {{-- <tr>
             <td>
                 <p style="text-align:left; font-size:10px; color:#4CAF50">Printed By: <font>
                         {{ Auth::user()->name }} </font>
@@ -35,10 +38,23 @@
                     @endif
                 </p>
             </td>
-        </tr>
+        </tr> --}}
         <tr>
-            <td colspan="3"style="text-align:center; font-style: italic; font-size:10px; color:#070707">
+            <td colspan="3"style="text-align:center;  font-size:10px; color:#070707">
                 <p>
+                    <span style="text-align:left; font-size:10px; color:#4CAF50">Printed By: <font>
+                            {{ Auth::user()->name }} </font>
+                    </span>
+                        <span style="text-align:center; font-size:10px; color:#4CAF50"> Print Date:
+                            {{ date('l d-M-Y H:i:s') }}
+                        </span>
+                        <span style="text-align:right; font-size:10px; color:#4CAF50"> Printed
+                            {{ $testResult->download_count }} time(s) @if ($testResult->tracker != '')
+                                [{{ $testResult->tracker }}]
+                            @endif
+                        </span>
+                </p>
+                <p style="font-style: italic;">
 
                     Website: <a style="color:#070707" href="https://gmi.mak.ac.ug">www.gmi.mak.ac.ug</a> |
                     Email: <a style="color:#070707" href="mailto:makbrc.chs@mak.ac.ug">makbrc.chs@mak.ac.ug</a> |
