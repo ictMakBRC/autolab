2ad29900464954a438e93778da426ce7fae30270
Ked <61323721+ked-k@users.noreply.github.com>
2025-04-08 15:25:46 +0300

    Update SystemReportItemsComponent.php

--- app/Http/Livewire/Admin/Reports/SystemReportItemsComponent.php
+++ app/Http/Livewire/Admin/Reports/SystemReportItemsComponent.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Livewire\Admin\Reports;
 
 use App\Models\Admin\SystemReport;
@@ -13,41 +12,41 @@ class SystemReportItemsComponent extends Component
     public $module;
     public $result;
     public $details;
-    public function mount($code){
-        $this->report = SystemReport::where('ref_code',$code)->first();
+    public function mount($code)
+    {
+        $this->report = SystemReport::where('ref_code', $code)->first();
     }
     public function storeData()
     {
         $this->validate([
-            'module' => 'required',
-            'score' => 'required',
-            'result' => 'required',
+            'module'  => 'required',
+            'score'   => 'required',
+            'result'  => 'required',
             'details' => 'nullable',
         ]);
 
-        $record = SystemReportItem::where(['system_report_id'=>$this->report->id, 'module' => $this->module])->firs();
-        if($record){
-            $this->dispatchBrowserEvent('alert', ['type' => 'warning',  'message' => 'Record already exists!']);
-            return;
-        }
-        $SystemReport = new SystemReportItem();    
+        // $record = SystemReportItem::where(['system_report_id'=>$this->report->id, 'module' => $this->module])->firs();
+        // if($record){
+        //     $this->dispatchBrowserEvent('alert', ['type' => 'warning',  'message' => 'Record already exists!']);
+        //     return;
+        // }
+        $SystemReport                   = new SystemReportItem();
         $SystemReport->system_report_id = $this->report->id;
-        $SystemReport->score = $this->score;
-        $SystemReport->result = $this->result;
-        $SystemReport->module = $this->module;
-        $SystemReport->details = $this->details;
+        $SystemReport->score            = $this->score;
+        $SystemReport->result           = $this->result;
+        $SystemReport->module           = $this->module;
+        $SystemReport->details          = $this->details;
         $SystemReport->save();
         $this->resetInputs();
-        $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'Record created successfully!']);
-  
+        $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Record created successfully!']);
+
     }
     public function saveReport()
     {
-        $this->report->update(['status'=>'Submitted']);
-        $this->dispatchBrowserEvent('alert', ['type' => 'success',  'message' => 'SystemReport created successfully!']);
+        $this->report->update(['status' => 'Submitted']);
+        $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'SystemReport created successfully!']);
         return redirect()->SignedRoute('qualityReportItems', $this->report->ref_code);
     }
-
 
     public function resetInputs()
     {
@@ -60,7 +59,7 @@ class SystemReportItemsComponent extends Component
     }
     public function render()
     {
-        $data['reportItems'] = SystemReportItem::where('system_report_id',  $this->report->id)->get();
+        $data['reportItems'] = SystemReportItem::where('system_report_id', $this->report->id)->get();
         return view('livewire.admin.reports.system-report-items-component', $data);
     }
 }
