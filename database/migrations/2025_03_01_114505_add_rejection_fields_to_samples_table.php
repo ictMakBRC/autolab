7d8815b7093474d93ea2f6b2679d954fdf563cc3
Ked <61323721+ked-k@users.noreply.github.com>
2025-04-09 18:06:39 +0300

    migration renaming

--- database/migrations/2025_03_01_114505_add_rejection_fields_to_samples_table.php
+++ database/migrations/2025_03_01_114505_add_rejection_fields_to_samples_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        // Schema::table('samples', function (Blueprint $table) {
-        //     $table->unsignedBigInteger('commented_by')->nullable()->after('status');
-        //     $table->text('sample_comment')->nullable()->after('status');
-        //     $table->dateTime('commented_at')->nullable()->after('status');
-        //     $table->text('rejection_reason')->nullable()->after('status');
-        // });
+        Schema::table('samples', function (Blueprint $table) {
+            $table->unsignedBigInteger('commented_by')->nullable()->after('status');
+            $table->text('sample_comment')->nullable()->after('status');
+            $table->dateTime('commented_at')->nullable()->after('status');
+            $table->text('rejection_reason')->nullable()->after('status');
+        });
 
         Schema::table('test_results', function (Blueprint $table) {
             $table->text('tat_comment')->nullable()->after('amended_at');
